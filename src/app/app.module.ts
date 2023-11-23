db84d9e497582ccdd0eeb28b79804965a7148f2a
Stan Tarnev <stan.tarnev@gmail.com>
2023-11-23 11:23:47 +0000

    simplified dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './components/messages.component';
+import { DashboardComponent } from './components/dashboard.component';
 
 @NgModule({
   declarations: [
